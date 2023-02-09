fb20497f210f5c4400335dcbd4ca89df74300286
reaeeun <raeeunshin@gmail.com>
2023-02-09 14:25:33 +0900

    raeeun

---
+++ src/main/java/com/diet/second_project_diet/SecondProjectDietApplication.java
@@ -0,0 +1,13 @@
+package com.diet.second_project_diet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondProjectDietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondProjectDietApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diet/second_project_diet/SecondProjectDietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diet.second_project_diet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondProjectDietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
