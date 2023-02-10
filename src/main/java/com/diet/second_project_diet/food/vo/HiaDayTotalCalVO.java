8524f3eda6e087dff18741eb1b28535faff4aa61
inwonjung <inwon123@naver.com>
2023-02-10 17:38:51 +0900

    inwon1

--- src/main/java/com/diet/second_project_diet/food/vo/HiaDayTotalCalVO.java
+++ src/main/java/com/diet/second_project_diet/food/vo/HiaDayTotalCalVO.java
@@ -3,7 +3,7 @@ package com.diet.second_project_diet.food.vo;
 import java.time.LocalDateTime;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import com.diet.second_project_diet.entity.DayFoodCompleteEntity;
+import com.diet.second_project_diet.entity.DayFoodEntity;
 
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
@@ -18,15 +18,14 @@ public class HiaDayTotalCalVO {
     private Long miSeq;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDateTime day;
-    private Integer goal;
-    private Integer totalCal;
+    
     
 
-// public HiaDayTotalCalVO(DayFoodCompleteEntity dfc){
-//     this.miSeq = dfc.getMember().getMiSeq();
-//     this.day = dfc.getDfcDate().toLocalDate();
-//     this.totalCal = dfc.getDfcTotalCal();
-// }
+public HiaDayTotalCalVO(DayFoodEntity dfc){
+    this.miSeq = dfc.getMember().getMiSeq();
+    // this.day = dfc.getDfDate().toLocalDate();
+   
+}
 
     
 }   
